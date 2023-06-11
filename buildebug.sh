e1ce7417c1454cc5d6ad447889f2bcabde46ec91
shenxx <shenxiangxiang@gmail.com>
2023-06-11 13:15:49 +0800

    update ksu

--- buildebug.sh
+++ buildebug.sh
@@ -3,11 +3,11 @@ MAIN=`readlink -f ${DIR}/..`
 
 #!/bin/bash
 # Resources
+make -j4 CC='ccache clang' ARCH=arm64 LLVM=1 LLVM_IAS=1 O=out gki_defconfig
+
 THREAD="-j4"
 
-export CROSS_COMPILE_COMPAT=/opt/clang-r416183b/bin/arm-linux-androidkernel- 
-export CROSS_COMPILE_ARM32=/opt/clang-r416183b/bin/arm-linux-androidkernel-
-export CLANG_TRIPLE=/opt/clang-r416183b/bin/aarch64-linux-gnu-
+export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=/opt/clang-r416183b/bin/aarch64-linux-gnu- CC=clang CXX=clang++
 export LD_LIBRARY_PATH=/opt/clang-r416183b/lib64:/usr/local/lib:$LD_LIBRARY_PATH
 DEFCONFIG="gki_defconfig"
@@ -29,12 +29,11 @@ echo "Making Kernel:"
 echo "-------------------"
 echo
 
-make CC='ccache clang' ARCH=arm64 LLVM=1 LLVM_IAS=1 O=out gki_defconfig
 make CC="ccache clang" CXX="ccache clang++" LLVM=1 LLVM_IAS=1 O=out $DEFCONFIG
 make CC='ccache clang' CXX="ccache clang++" LLVM=1 LLVM_IAS=1 O=out $THREAD \
     CONFIG_MEDIATEK_CPUFREQ_DEBUG=m CONFIG_MTK_IPI=m CONFIG_MTK_TINYSYS_MCUPM_SUPPORT=m \
     CONFIG_MTK_MBOX=m CONFIG_RPMSG_MTK=m CONFIG_LTO_CLANG=y CONFIG_LTO_NONE=n \
-    CONFIG_LTO_CLANG_THIN=y CONFIG_LTO_CLANG_FULL=n  2>&1 | tee kernel.log
+    CONFIG_LTO_CLANG_THIN=y CONFIG_LTO_CLANG_FULL=n 2>&1 | tee kernel.log
 
 echo
 echo "-------------------"
